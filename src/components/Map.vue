a13ca437a0af0d77fbe879e5d9a70f7e1f29d4d6
Chris Vaillancourt <cvaillan22@gmail.com>
2019-12-10 13:58:42 -0700

    added vector tile basemap layers

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -9,11 +9,36 @@ export default {
   name: "web-map",
   mounted() {
     // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(["esri/Map", "esri/views/MapView", "esri/widgets/Home"], {
-      css: true,
-    }).then(([Map, MapView, Home]) => {
+    loadModules(
+      [
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/widgets/Home",
+        "esri/layers/VectorTileLayer",
+        "esri/Basemap",
+      ],
+      {
+        css: true,
+      }
+    ).then(([Map, MapView, Home, VectorTileLayer, Basemap]) => {
+      const vectorBaseLayer = new VectorTileLayer({
+        // item page: https://www.arcgis.com/home/item.html?id=291da5eab3a0412593b66d384379f89f
+        url:
+          "https://www.arcgis.com/sharing/rest/content/items/291da5eab3a0412593b66d384379f89f/resources/styles/root.json",
+      });
+      const vectorBaseReference = new VectorTileLayer({
+        // item page: https://www.arcgis.com/home/item.html?id=1768e8369a214dfab4e2167d5c5f2454
+        url:
+          "https://www.arcgis.com/sharing/rest/content/items/1768e8369a214dfab4e2167d5c5f2454/resources/styles/root.json",
+      });
+
+      const basemap = new Basemap({
+        baseLayers: [vectorBaseLayer],
+        referenceLayers: [vectorBaseReference],
+      });
+
       const map = new Map({
-        basemap: "streets",
+        basemap,
       });
       const view = new MapView({
         container: this.$el,
@@ -21,6 +46,7 @@ export default {
         zoom: 8,
         center: [-118, 34],
       });
+
       const homeButton = new Home({
         view,
       });
